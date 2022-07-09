5d53a9e58e98aed1fa4154ce651a4e8028470fcb
Moises Santos <moisesbarbosa23@gmail.com>
2022-07-09 12:49:02 -0300

    add: findAll and findById

---
+++ src/main/java/com/example/springproject/Services/UserService.java
@@ -0,0 +1,24 @@
+package com.example.springproject.Services;
+
+import com.example.springproject.Entities.User;
+import com.example.springproject.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
