b1dab41c303e03e657212c0c1f7156e5d2e08f76
Moises Santos <moisesbarbosa23@gmail.com>
2022-07-11 09:50:46 -0300

    add: orderStatus enum

--- src/main/java/com/example/springproject/Entities/Order.java
+++ src/main/java/com/example/springproject/Entities/Order.java
@@ -1,5 +1,6 @@
 package com.example.springproject.Entities;
 
+import com.example.springproject.Entities.Enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -16,16 +17,19 @@ public class Order implements Serializable {
     private Long id;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
+
+    private Integer orderStatus;
     @ManyToOne
     @JoinColumn(name = "client_Id")
     private User client;
 
     public Order() {};
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(orderStatus);
     }
 
     public Long getId() {
@@ -50,6 +54,16 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(this.orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
--- src/main/java/com/example/springproject/config/TestConfig.java
+++ src/main/java/com/example/springproject/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.example.springproject.config;
 
+import com.example.springproject.Entities.Enums.OrderStatus;
 import com.example.springproject.Entities.Order;
 import com.example.springproject.Entities.User;
 import com.example.springproject.Repositories.OrderRepository;
@@ -24,10 +25,10 @@ public class TestConfig implements CommandLineRunner {
     public void run(String... args) throws Exception {
         User user1 = new User(null, "Maria Brown", "maria@mail.com", "999999", "1234567");
         User user2 = new User(null, "Alex Green", "alex@mail.com", "888888", "123456");
-        Order o1 = new Order(null, Instant.parse("2022-07-07T19:00:00Z"), user1);
-        Order o2 = new Order(null, Instant.parse("2022-07-06T15:00:00Z"), user2);
-        Order o3 = new Order(null, Instant.parse("2022-07-05T12:00:00Z"), user1);
-        Order o4 = new Order(null, Instant.parse("2022-07-06T08:00:00Z"), user2);
+        Order o1 = new Order(null, Instant.parse("2022-07-07T19:00:00Z"), OrderStatus.PAID, user1);
+        Order o2 = new Order(null, Instant.parse("2022-07-06T15:00:00Z"), OrderStatus.DELIVERED, user2);
+        Order o3 = new Order(null, Instant.parse("2022-07-05T12:00:00Z"),OrderStatus.DELIVERED, user1);
+        Order o4 = new Order(null, Instant.parse("2022-07-06T08:00:00Z"), OrderStatus.WAITING_PAYMENT, user2);
 
         userRepository.saveAll(Arrays.asList(user1, user2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3, o4));
