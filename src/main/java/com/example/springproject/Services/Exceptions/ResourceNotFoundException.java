6a017625f4bf0526906117403e0178c19e8d33f8
Moises Santos <moisesbarbosa23@gmail.com>
2022-07-12 09:57:12 -0300

    add: exception treatment when searching for id

---
+++ src/main/java/com/example/springproject/Services/Exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.example.springproject.Services.Exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+    }
+}
--- src/main/java/com/example/springproject/Services/UserService.java
+++ src/main/java/com/example/springproject/Services/UserService.java
@@ -2,6 +2,7 @@ package com.example.springproject.Services;
 
 import com.example.springproject.Entities.User;
 import com.example.springproject.Repositories.UserRepository;
+import com.example.springproject.Services.Exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -19,7 +20,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user) {
